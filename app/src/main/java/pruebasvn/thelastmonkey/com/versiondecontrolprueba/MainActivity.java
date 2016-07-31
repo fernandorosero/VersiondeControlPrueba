9476824b1798d6ca2e8fed4fcd84c144e79bb78a
fernandorosero <tatofarp@hotmail.com>
2016-07-31 14:52:07 +0200

    Commit 1 Toshiba i5

--- app/src/main/java/pruebasvn/thelastmonkey/com/versiondecontrolprueba/MainActivity.java
+++ app/src/main/java/pruebasvn/thelastmonkey/com/versiondecontrolprueba/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                Toast.makeText(getApplicationContext(),"Saludos desde el click de la app",Toast.LENGTH_SHORT).show();
-               Toast.makeText(MainActivity.this, "Saludines", Toast.LENGTH_SHORT).show();
+               Toast.makeText(MainActivity.this, "Mejor otro boton", Toast.LENGTH_SHORT).show();
             }
         });
 
