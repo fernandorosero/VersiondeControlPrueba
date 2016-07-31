5fe66a0646a443e8c4fff7a363a58162504486ba
fernandorosero <tatofarp@hotmail.com>
2016-07-31 14:35:35 +0200

    Commit 1 Toshiba i5

--- app/src/main/java/pruebasvn/thelastmonkey/com/versiondecontrolprueba/MainActivity.java
+++ app/src/main/java/pruebasvn/thelastmonkey/com/versiondecontrolprueba/MainActivity.java
@@ -2,12 +2,27 @@ package pruebasvn.thelastmonkey.com.versiondecontrolprueba;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSaludos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSaludos.findViewById(R.id.btnSaluditos);
+
+        btnSaludos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               // Toast.makeText(getApplicationContext(),"Saludos desde el click de la app",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
